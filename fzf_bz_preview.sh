055dee82b8bca037a6d20e31e767eac6c81b516b
Roman Nefyodov <roman.nefyodov@gmail.com>
2024-08-17 17:58:29 +0300

    better

--- fzf_bz_preview.sh
+++ fzf_bz_preview.sh
@@ -7,4 +7,8 @@ line=${remaining%%:*}
 left=$(( line - 5 > 0 ? line - 5 : 0 ))
 right=$(( line + 10 ))
 
-bat "$file" -H "$line" -r $left:$right -l bazel -f --theme=ansi --style=numbers,header
+if which bat &> /dev/null; then
+  bat "$file" -H "$line" -r $left:$right -l bazel -f --theme=ansi --style=numbers,header
+else
+  nl -ba "$file" | sed -n "$left,${right}p"
+fi
